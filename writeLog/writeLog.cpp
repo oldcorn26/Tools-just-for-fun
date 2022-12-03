7652acffde9ce9dae8ef9e9a8ef20aead1d64e5b
CornLiu <74982782+oldcorn26@users.noreply.github.com>
2022-12-03 15:33:38 +0800

    Enhance robustness.

--- writeLog/writeLog.cpp
+++ writeLog/writeLog.cpp
@@ -10,15 +10,15 @@
  * @param fileAddress is the address we save log file.
  * @return 0 if success and 1 if fail.
  */
-int WriteLog(string &msg, string &fileAddress) {
+int WriteLog(const string &msg, const string &fileAddress) {
     SYSTEMTIME sys;
-    GetLocalTime(&sys); //取系统时间
+    GetLocalTime(&sys); //Get system time.
     stringstream strTemp;
     strTemp << sys.wMonth << '.' << sys.wDay << " " << sys.wHour << ':'
             << sys.wMinute << ':' << sys.wSecond << ':' << sys.wMilliseconds << "  ";
 
     ofstream outfile;
-//    outfile.open(R"(c:\users\corn\desktop\test\LOG.txt)", ios::app); //文件的物理地址，文件的打开方式
+//    outfile.open(R"(c:\users\corn\desktop\test\LOG.txt)", ios::app); //The adress we want to save and open method.
     outfile.open(fileAddress, ios::app);
     if (outfile.is_open()) {
         outfile << strTemp.str() << msg << "\n";
