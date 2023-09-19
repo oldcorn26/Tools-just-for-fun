4cd1908fd524c469a06193a18fdbd9094d00b4b0
Corn Liu <74982782+oldcorn26@users.noreply.github.com>
2023-09-20 04:59:47 +0800

    Create TradesNumberMonitor

---
+++ TradesNumberMonitor/trades_number_monitor.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <vector>
+#include "LoopQueue.h"
+
+/*
+ * TradesNumberMonitor class is used for monitoring trades number.
+ * periods must be sorted in descending order.
+ */
+
+class TradesNumberMonitor {
+public:
+    TradesNumberMonitor(const std::vector<int> &periods, const std::vector<int> &thresholds);
+    void add(int trades);
+    int peakSum(size_t idx);
+    bool checkIfAlert(size_t idx);
+
+private:
+    int maxPeriod;
+    std::vector<int> periods;
+    std::vector<int> thresholds;
+    std::vector<int> sums;
+    LoopQueue<int> buckets;
+};
+
+TradesNumberMonitor::TradesNumberMonitor(const std::vector<int> &periods, const std::vector<int> &thresholds)
+        : periods(periods), thresholds(thresholds), maxPeriod(*std::max_element(periods.begin(), periods.end())),
+          buckets(maxPeriod), sums(periods.size(), 0) {
+}
+
+void TradesNumberMonitor::add(int trades) {
+    if (buckets.full()) {
+        for (size_t i = 0; i < periods.size(); ++i) {
+            sums[i] -= buckets.peakFrontI(maxPeriod - periods[i]);
+        }
+    }
+
+    buckets.put(trades);
+
+    for (size_t i = 0; i < periods.size(); ++i) {
+        if (buckets.full() || periods[i] + buckets.size() > maxPeriod) {
+            sums[i] += trades;
+        }
+    }
+}
+
+int TradesNumberMonitor::peakSum(size_t idx) {
+    if (idx >= sums.size()) {
+        throw std::out_of_range("Index out of range");
+    }
+    return sums[idx];
+}
+
+bool TradesNumberMonitor::checkIfAlert(size_t idx) {
+    if (idx >= thresholds.size()) {
+        throw std::out_of_range("Index out of range");
+    }
+    return sums[idx] >= thresholds[idx];
+}
