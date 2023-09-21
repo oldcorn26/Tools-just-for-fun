b50deb1e9a437220e3883bbc4df21f1dfc463ecf
Corn Liu <74982782+oldcorn26@users.noreply.github.com>
2023-09-21 16:15:27 +0800

    Create websocketClient

---
+++ WebsocketClient/websocket_clinet.h
@@ -0,0 +1,182 @@
+#pragma once
+
+#include <string>
+#include <thread>
+#include <functional>
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/asio/strand.hpp>
+#include <boost/beast/ssl.hpp>
+#include <boost/beast/websocket/ssl.hpp>
+#include "libs/nlohmann/json.hpp"
+
+namespace beast = boost::beast;         // from <boost/beast.hpp>
+namespace http = beast::http;           // from <boost/beast/http.hpp>
+namespace websocket = beast::websocket; // from <boost/beast/websocket.hpp>
+namespace net = boost::asio;            // from <boost/asio.hpp>
+namespace ssl = boost::asio::ssl;       // from <boost/asio/ssl.hpp>
+using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
+
+// Sends a WebSocket message and prints the response
+class Websocket : public std::enable_shared_from_this<Websocket> {
+    tcp::resolver resolver_;
+    websocket::stream<beast::ssl_stream<beast::tcp_stream>> ws_;
+    beast::flat_buffer buffer_;
+    std::string host_;
+    std::string text_;
+    std::function<void(const nlohmann::json&)> callback_;
+
+public:
+    explicit Websocket(net::io_context &ioc, ssl::context& ctx, std::function<void(const nlohmann::json &)> callback)
+            : resolver_(net::make_strand(ioc)), ws_(net::make_strand(ioc), ctx), callback_(callback) {}
+
+    void run(char const* host, char const* port, char const* text);
+    void on_resolve(beast::error_code ec, tcp::resolver::results_type results);
+    void on_connect(beast::error_code ec, tcp::resolver::results_type::endpoint_type ep);
+    void on_ssl_handshake(beast::error_code ec);
+    void on_handshake(beast::error_code ec);
+    void on_write(beast::error_code ec, std::size_t bytes_transferred);
+    void on_read(beast::error_code ec, std::size_t bytes_transferred);
+    void on_close(beast::error_code ec);
+};
+
+void load_root_certificates(ssl::context &ctx) {
+    std::string const cert = "-----BEGIN CERTIFICATE-----\n"
+                             "MIIH0zCCBbugAwIBAgIIXsO3pkN/pOAwDQYJKoZIhvcNAQEFBQAwQjESMBAGA1UE\n"
+                             "AwwJQUNDVlJBSVoxMRAwDgYDVQQLDAdQS0lBQ0NWMQ0wCwYDVQQKDARBQ0NWMQsw\n"
+                             "CQYDVQQGEwJFUzAeFw0xMTA1MDUwOTM3MzdaFw0zMDEyMzEwOTM3MzdaMEIxEjAQ\n"
+                             "BgNVBAMMCUFDQ1ZSQUlaMTEQMA4GA1UECwwHUEtJQUNDVjENMAsGA1UECgwEQUND\n"
+                             "VjELMAkGA1UEBhMCRVMwggIiMA0GCSqGSIb3DQEBAQUAA4ICDwAwggIKAoICAQCb\n"
+                             "qau/YUqXry+XZpp0X9DZlv3P4uRm7x8fRzPCRKPfmt4ftVTdFXxpNRFvu8gMjmoY\n"
+                             "HtiP2Ra8EEg2XPBjs5BaXCQ316PWywlxufEBcoSwfdtNgM3802/J+Nq2DoLSRYWo\n"
+                             "G2ioPej0RGy9ocLLA76MPhMAhN9KSMDjIgro6TenGEyxCQ0jVn8ETdkXhBilyNpA\n"
+                             "lHPrzg5XPAOBOp0KoVdDaaxXbXmQeOW1tDvYvEyNKKGno6e6Ak4l0Squ7a4DIrhr\n"
+                             "IA8wKFSVf+DuzgpmndFALW4ir50awQUZ0m/A8p/4e7MCQvtQqR0tkw8jq8bBD5L/\n"
+                             "0KIV9VMJcRz/RROE5iZe+OCIHAr8Fraocwa48GOEAqDGWuzndN9wrqODJerWx5eH\n"
+                             "k6fGioozl2A3ED6XPm4pFdahD9GILBKfb6qkxkLrQaLjlUPTAYVtjrs78yM2x/47\n"
+                             "4KElB0iryYl0/wiPgL/AlmXz7uxLaL2diMMxs0Dx6M/2OLuc5NF/1OVYm3z61PMO\n"
+                             "m3WR5LpSLhl+0fXNWhn8ugb2+1KoS5kE3fj5tItQo05iifCHJPqDQsGH+tUtKSpa\n"
+                             "cXpkatcnYGMN285J9Y0fkIkyF/hzQ7jSWpOGYdbhdQrqeWZ2iE9x6wQl1gpaepPl\n"
+                             "uUsXQA+xtrn13k/c4LOsOxFwYIRKQ26ZIMApcQrAZQIDAQABo4ICyzCCAscwfQYI\n"
+                             "KwYBBQUHAQEEcTBvMEwGCCsGAQUFBzAChkBodHRwOi8vd3d3LmFjY3YuZXMvZmls\n"
+                             "ZWFkbWluL0FyY2hpdm9zL2NlcnRpZmljYWRvcy9yYWl6YWNjdjEuY3J0MB8GCCsG\n"
+                             "AQUFBzABhhNodHRwOi8vb2NzcC5hY2N2LmVzMB0GA1UdDgQWBBTSh7Tj3zcnk1X2\n"
+                             "VuqB5TbMjB4/vTAPBgNVHRMBAf8EBTADAQH/MB8GA1UdIwQYMBaAFNKHtOPfNyeT\n"
+                             "VfZW6oHlNsyMHj+9MIIBcwYDVR0gBIIBajCCAWYwggFiBgRVHSAAMIIBWDCCASIG\n"
+                             "CCsGAQUFBwICMIIBFB6CARAAQQB1AHQAbwByAGkAZABhAGQAIABkAGUAIABDAGUA\n"
+                             "cgB0AGkAZgBpAGMAYQBjAGkA8wBuACAAUgBhAO0AegAgAGQAZQAgAGwAYQAgAEEA\n"
+                             "QwBDAFYAIAAoAEEAZwBlAG4AYwBpAGEAIABkAGUAIABUAGUAYwBuAG8AbABvAGcA\n"
+                             "7QBhACAAeQAgAEMAZQByAHQAaQBmAGkAYwBhAGMAaQDzAG4AIABFAGwAZQBjAHQA\n"
+                             "cgDzAG4AaQBjAGEALAAgAEMASQBGACAAUQA0ADYAMAAxADEANQA2AEUAKQAuACAA\n"
+                             "QwBQAFMAIABlAG4AIABoAHQAdABwADoALwAvAHcAdwB3AC4AYQBjAGMAdgAuAGUA\n"
+                             "czAwBggrBgEFBQcCARYkaHR0cDovL3d3dy5hY2N2LmVzL2xlZ2lzbGFjaW9uX2Mu\n"
+                             "aHRtMFUGA1UdHwROMEwwSqBIoEaGRGh0dHA6Ly93d3cuYWNjdi5lcy9maWxlYWRt\n"
+                             "aW4vQXJjaGl2b3MvY2VydGlmaWNhZG9zL3JhaXphY2N2MV9kZXIuY3JsMA4GA1Ud\n"
+                             "DwEB/wQEAwIBBjAXBgNVHREEEDAOgQxhY2N2QGFjY3YuZXMwDQYJKoZIhvcNAQEF\n"
+                             "BQADggIBAJcxAp/n/UNnSEQU5CmH7UwoZtCPNdpNYbdKl02125DgBS4OxnnQ8pdp\n"
+                             "D70ER9m+27Up2pvZrqmZ1dM8MJP1jaGo/AaNRPTKFpV8M9xii6g3+CfYCS0b78gU\n"
+                             "JyCpZET/LtZ1qmxNYEAZSUNUY9rizLpm5U9EelvZaoErQNV/+QEnWCzI7UiRfD+m\n"
+                             "AM/EKXMRNt6GGT6d7hmKG9Ww7Y49nCrADdg9ZuM8Db3VlFzi4qc1GwQA9j9ajepD\n"
+                             "vV+JHanBsMyZ4k0ACtrJJ1vnE5Bc5PUzolVt3OAJTS+xJlsndQAJxGJ3KQhfnlms\n"
+                             "tn6tn1QwIgPBHnFk/vk4CpYY3QIUrCPLBhwepH2NDd4nQeit2hW3sCPdK6jT2iWH\n"
+                             "7ehVRE2I9DZ+hJp4rPcOVkkO1jMl1oRQQmwgEh0q1b688nCBpHBgvgW1m54ERL5h\n"
+                             "I6zppSSMEYCUWqKiuUnSwdzRp+0xESyeGabu4VXhwOrPDYTkF7eifKXeVSUG7szA\n"
+                             "h1xA2syVP1XgNce4hL60Xc16gwFy7ofmXx2utYXGJt/mwZrpHgJHnyqobalbz+xF\n"
+                             "d3+YJ5oyXSrjhO7FmGYvliAd3djDJ9ew+f7Zfc3Qn48LFFhRny+Lwzgt3uiP1o2H\n"
+                             "pPVWQxaZLPSkVrQ0uGE3ycJYgBugl6H8WY3pEfbRD0tVNEYqi4Y7\n"
+                             "-----END CERTIFICATE-----\n";
+
+    ctx.add_certificate_authority(boost::asio::buffer(cert.data(), cert.size()));
+}
+
+void Websocket::run(char const *host, char const *port, char const *text) {
+    host_ = host;
+    text_ = text;
+
+    resolver_.async_resolve(
+            host,
+            port,
+            beast::bind_front_handler(&Websocket::on_resolve, shared_from_this()));
+}
+
+void Websocket::on_resolve(beast::error_code ec, tcp::resolver::results_type results) {
+    if (ec)
+        return;
+
+    beast::get_lowest_layer(ws_).async_connect(
+            results,
+            beast::bind_front_handler(&Websocket::on_connect, shared_from_this()));
+}
+
+void Websocket::on_connect(beast::error_code ec, tcp::resolver::results_type::endpoint_type ep) {
+    if (ec)
+        return;
+
+    if (!SSL_set_tlsext_host_name(
+            ws_.next_layer().native_handle(),
+            host_.c_str())) {
+        ec = beast::error_code(static_cast<int>(::ERR_get_error()),
+                               net::error::get_ssl_category());
+        return;
+    }
+
+    host_ += ':' + std::to_string(ep.port());
+
+    ws_.next_layer().async_handshake(
+            ssl::stream_base::client,
+            beast::bind_front_handler(&Websocket::on_ssl_handshake, shared_from_this()));
+}
+
+void Websocket::on_ssl_handshake(beast::error_code ec) {
+    if (ec)
+        return;
+
+    ws_.async_handshake(host_,
+                        "/ws",
+                        beast::bind_front_handler(&Websocket::on_handshake, shared_from_this()));
+}
+
+void Websocket::on_handshake(beast::error_code ec) {
+    if (ec)
+        return;
+
+    ws_.async_write(
+            net::buffer(text_),
+            beast::bind_front_handler(&Websocket::on_write, shared_from_this()));
+}
+
+void Websocket::on_write(beast::error_code ec, std::size_t bytes_transferred) {
+    boost::ignore_unused(bytes_transferred);
+
+    if (ec)
+        return;
+
+    ws_.async_read(
+            buffer_,
+            beast::bind_front_handler(&Websocket::on_read, shared_from_this()));
+}
+
+void Websocket::on_read(beast::error_code ec, std::size_t bytes_transferred) {
+    boost::ignore_unused(bytes_transferred);
+
+    if (ec)
+        return;
+
+    auto data = buffer_.data();
+    auto pointer = static_cast<const char *>(data.data());
+    auto len = data.size();
+    auto ret = nlohmann::json::parse(pointer, pointer + len);
+
+    callback_(ret);
+
+    buffer_.consume(buffer_.size());
+
+    ws_.async_read(
+            buffer_,
+            beast::bind_front_handler(&Websocket::on_read, shared_from_this()));
+}
+
+void Websocket::on_close(beast::error_code ec) {
+    if (ec)
+        return;
+}
